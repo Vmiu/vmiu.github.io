94726ad1bac57bb2fe6e0a3395ffea799bd58116
Vmiu <miuvictor21079@gmail.com>
2025-07-19 13:07:22 +0800

    solve deployment bug caused by incorrect config

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://vmiu.github.io",
-	base: "vmiu.github.io",
+	// base: "vmiu.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
